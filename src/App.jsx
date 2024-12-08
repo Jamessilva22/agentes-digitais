656c1758cfadd3231015aef05d6c07233424177b
james silva <jamessilvanoel@gmail.com>
2024-12-08 15:13:02 -0300

    Tela de consulta adicionada como rota

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Importa os componentes de roteamento
 import BeneficiarioForm from "./components/BeneficiarioForm";
 import Login from "./components/login";
+import ConsultaCadastro from "./components/consultaBeneficiario";
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/beneficiario" element={<BeneficiarioForm />} />
           <Route path="/tela-escolha" element={<telaEscolha />} />
+
+          <Route path="/consulta" element={<ConsultaCadastro/>}/>
         </Routes>
       </div>
     </Router>
